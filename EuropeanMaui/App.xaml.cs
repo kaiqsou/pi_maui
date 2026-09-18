a82bf318eb834d83920a6353ade80ffa8db8d8e4
Kaique <kaiqsouza2004@hotmail.com>
2026-09-17 21:14:19 -0300

    Inicialização do projeto com Maui

---
+++ EuropeanMaui/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace EuropeanMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var janela = new Window(new AppShell());
+            janela.Width = 400;
+            janela.Height = 750;
+
+            return janela;
+        }
+    }
+}
---
+++ EuropeanMaui/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "EuropeanMaui")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "EuropeanMaui.Pages")]
